f3331ac96f9e3195233c4fa738401e66a3cd705e
victoria parchevskaya <parchevskayavictoria@gmail.com>
2023-02-06 13:21:07 +0300

    feat: add sout develop3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("develop 1");
         System.out.println("develop 2");
+        System.out.println("develop 3");
     }
 }
