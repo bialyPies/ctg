4449df910798c7f0fe6c8023720e9611a3e06f50
victoria parchevskaya <parchevskayavictoria@gmail.com>
2023-02-08 02:51:07 +0300

    feat: add 3rd step to feature1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     static public void feature1(){
         System.out.println("feature1 step 1");
         System.out.println("feature1 step 2");
+        System.out.println("feature1 step 3");
     }
     public static void main(String[] args) {
         System.out.println("Hello world!");
