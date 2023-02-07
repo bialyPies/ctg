3e86de8f64c189d210e9f2e7534b7f7e30578f01
victoria parchevskaya <parchevskayavictoria@gmail.com>
2023-02-07 23:57:21 +0300

    feat: add 1st step to feature2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
 public class Main {
+
+    static public void feature2(){
+        System.out.println("feature2 step 1");
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("develop 1");
